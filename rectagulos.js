dea264cf16664e7a9d504ff93a1d63012277d3f3
Eve_isla_2506 <119141420+Emiliano2506@users.noreply.github.com>
2024-02-03 12:21:01 -0600

    Add files via upload

---
+++ rectagulos.js
@@ -0,0 +1,13 @@
+class Rectangulo{
+    constructor(){
+        this.x=15;
+        this.y=10;
+        this.an=30;
+        this.al=15;
+    }
+    display(){
+        console.log("espero y funcione-58-");
+        fill("red");
+        rect(this.x,this.y,this.an,this.al);
+    }
+}
